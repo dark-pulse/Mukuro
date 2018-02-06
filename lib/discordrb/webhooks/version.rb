934eba0e5a973bd3ca5a65f3d69d36f80326fc59
Venus <Venus>
2018-02-06 20:04:31 +0200

    2

--- lib/discordrb/webhooks/version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Webhook support for discordrb
-module Discordrb
-  module Webhooks
-    # The current version of discordrb-webhooks.
-    VERSION = '3.2.0.1'.freeze
-  end
-end
