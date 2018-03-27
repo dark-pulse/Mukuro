ee85fb68ee4414eb5242e1ad527cded345e2cefe
Venus <Venus>
2018-03-27 21:47:14 +0200

    1

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require 'dotenv'
 Dotenv.load
 
 require 'discordrb'
-bot = Discordrb::Commands::CommandBot.new token: 'NDE5MjgwMzk0MjUzNTAwNDI2.DYCgFA.c6Gxg0O07jDKEnlP-EG8t7B25lY', application_id: '419280394253500426', prefix: 's!', advanced_functionality: false, debug: true #, log_mode: :debug
+bot = Discordrb::Commands::CommandBot.new token: 'NDE5MjgwMzk0MjUzNTAwNDI2.DYCgFA.c6Gxg0O07jDKEnlP-EG8t7B25lY', application_id: '419280394253500426', prefix: 's!', advanced_functionality: false, debug: true #, log_modes: :debug
 
 dir = File.dirname(__FILE__)
 
