4da650086652148f7e66dadecd090f6135f849d1
Venus <Venus>
2018-05-16 21:04:23 +0200

    mmm

--- Interrogated.rb
+++ Interrogated.rb
@@ -3,10 +3,8 @@ require 'discordrb'
 TOKEN = 'NDQyNzE2ODgxMDI4MTIwNjAw.DdDGiQ.01JdGgsVHAtcFCzPgOGZu26yya4'
 bot = Discordrb::Bot.new token: TOKEN
 
-bot.message(in: 442799943175110676) do |event|
-    unless event.user.id == [271097183292489729, 442709871947677696]
+bot.message(in: 442799943175110676, from: "LykosHand") do |event|
         bot.send_message(442799827764641814, "#{event.message.content}")
-    end
 end
 
 bot.run
--- Interrogator.rb
+++ Interrogator.rb
@@ -3,10 +3,8 @@ require 'discordrb'
 TOKEN = 'NDQyNzA5ODcxOTQ3Njc3Njk2.DdDGXA.Ede_TKm0_LFyFMwEPWJ2m5VWt6o'
 bot = Discordrb::Bot.new token: TOKEN
 
-bot.message(in: 442799827764641814) do |event|
-    unless event.user.id == [271097183292489729, 442716881028120600]
+bot.message(in: 442799827764641814, from: "Nhankiet") do |event|
     bot.send_message(442799943175110676, "#{event.message.content}")
-    end
 end
 
 bot.run
