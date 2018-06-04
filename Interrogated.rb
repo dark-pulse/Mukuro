f797892c37e90d6631f0efd979efc0b240b17fc4
Venus <Venus>
2018-06-04 17:34:10 +0200

    No-Candies Graveyard

--- Interrogated.rb
+++ Interrogated.rb
@@ -3,7 +3,7 @@ require 'discordrb'
 TOKEN = 'NDQyNzE2ODgxMDI4MTIwNjAw.DdDGiQ.01JdGgsVHAtcFCzPgOGZu26yya4'
 bot = Discordrb::Bot.new token: TOKEN
 
-bot.message(in: 442799943175110676, from: "LykosHand") do |event|
+bot.message(in: 442799943175110676, from: "Candy") do |event|
         bot.send_message(442799827764641814, "#{event.message.content}")
 end
 
