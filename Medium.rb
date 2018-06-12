0c50fd50a2a9f715f92e237acfaa295c86650179
Venus <Venus>
2018-06-13 00:39:13 +0200

    123 4!

--- Medium.rb
+++ Medium.rb
@@ -1,16 +1,11 @@
 require 'discordrb'
 
-TOKEN = 'NDQyNDMxNDI4MTk4NjYyMTQ0.DdDErA.KQOxXT_1Q62m8gMIu4mKb4TuawI'
+TOKEN = 'NDU2MjI1MzY2Njk3MDUwMTEy.DgHchw.YIB2k3HZvyzZgHHDiHw-eJV6vog'
 bot = Discordrb::Bot.new token: TOKEN
 
-bot.message(in: 442724139015471104) do |event| 
-    if event.user.id == 271097183292489729
-        # Code
-    elsif event.user.id == 442430088659599370
-        # Code
-    else
-        bot.send_message(442723126045048832, "#{event.message.content}") 
-    end
+bot.ready do |event|
+	bot.dnd
+	bot.game = "Awaiting configurations..."
 end
 
 bot.run
